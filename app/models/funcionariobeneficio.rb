623415ddd40465f4daeab7430b240d1df08d1ed6
Kenia Guimaraes <keniiarruda@gmail.com>
2022-05-17 02:50:55 -0300

    Adiciona layout do dashboard.

--- app/models/funcionariobeneficio.rb
+++ app/models/funcionariobeneficio.rb
@@ -3,9 +3,9 @@ class Funcionariobeneficio < ApplicationRecord
     belongs_to:funcionario
 
     validates :beneficio_id, presence: {message: 'Informe um Plano!'}
-    validates :altura, presence: {message: 'Informar Altura'},numericality:{greater_than:0},  if: :campos2   #{message: 'Informar Altura'}
-    validates :peso, presence: {message:   'Informar Peso'}, numericality:{greater_than: 0} ,if: :campos2# {message:   'Informar Peso'}
-    validates :horas_meditadas, presence: {message: 'Informar Horas Meditadas'}, if: :campos #{message: 'Informar Horas Meditadas'}
+    validates :altura, presence: {message: 'Informar Altura'},numericality:{greater_than:0},  if: :campos2   
+    validates :peso, presence: {message:   'Informar Peso'}, numericality:{greater_than: 0} ,if: :campos2
+    validates :horas_meditadas, presence: {message: 'Informar Horas Meditadas'}, if: :campos 
       
     def campos
         beneficio_id == 4 
